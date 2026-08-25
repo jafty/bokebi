b30b98eb4e6f965172caaebdaebb58b2e7a68a5f
jafty <japhet.situmonana@gmail.com>
2026-08-25 18:56:18 +0200

    Share participation link from results

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from django.utils import timezone
 from surveys.models import SurveyRecord
 
 @pytest.mark.django_db
@@ -9,3 +10,20 @@ def test_home_and_create_pages_render(client):
 @pytest.mark.django_db
 def test_unknown_survey_is_not_found(client):
     assert client.get(reverse("take-survey", args=["missing"])).status_code == 404
+
+
+@pytest.mark.django_db
+def test_results_share_link_points_to_participation_page(client):
+    survey = SurveyRecord.objects.create(
+        public_id="team-survey",
+        team_name="Team",
+        deletion_key_hash="unused",
+        created_at=timezone.now(),
+    )
+
+    response = client.get(reverse("results", args=[survey.public_id]))
+
+    expected_url = f"http://testserver{reverse('take-survey', args=[survey.public_id])}"
+    assert response.status_code == 200
+    assert response.context["share_url"] == expected_url
+    assert response.content.count(expected_url.encode()) == 2
