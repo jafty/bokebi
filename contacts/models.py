94645df73e40359a7a60d6135073220ba2ff7051
jafty <japhet.situmonana@gmail.com>
2026-08-25 18:55:19 +0200

    Remove contact opt-in timestamps

--- contacts/models.py
+++ contacts/models.py
@@ -4,7 +4,6 @@ class ContactRequestRecord(models.Model):
     email = models.EmailField()
     wants_colleagues = models.BooleanField(default=False)
     wants_organization = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         app_label = "contacts"
